979c7f6e65f80689d5103bfdea68ffd704c00ace
Kingsley <kingsleyihemelanduofficial@gmail.com>
2022-09-16 07:30:17 +0100

    Add Allowed Host

--- renestitches/settings.py
+++ renestitches/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-yw+t(7pk1wp8i2n)5h#$69eqo&vl^if(nfpz2hiucw7nf$htlo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["http://localhost:8000", "kingsleyjr.pythonanywhere.com"]
 
 
 # Application definition
--- renestitches/wsgi.py
+++ renestitches/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for renestitches project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'renestitches.settings')
+path = "/home/kingsleyjr/ReneStitches"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "renestiches.settings"
+
+# then:
 
 application = get_wsgi_application()
